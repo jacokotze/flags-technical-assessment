0e6343a40aca14d1be2aa5a1523cf51487214566
Jaco Kotze <jaco@tct.digital>
2022-08-12 11:19:31 +0200

    changed routes. Root route points to flags resource

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::resource('/flags', FlagsController::class);
+Route::resource('/', FlagsController::class);
 
